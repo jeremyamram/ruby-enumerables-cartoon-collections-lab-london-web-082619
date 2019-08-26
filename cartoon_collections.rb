07bef752daf70aaaa821e909c50b86f57c0833ee
Jeremy Amram <jeremy.j.amram@gmail.com>
2019-08-26 18:01:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-planeteer_calls.map{|string| string.capitalize}
+planeteer_calls.map{|string| "#{string.capitalize}!"}
 end
 
 def long_planeteer_calls(short_words)
